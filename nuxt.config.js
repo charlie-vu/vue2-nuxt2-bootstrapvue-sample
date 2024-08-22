f4aa57542ed1235ac637a0019dd801a4a319a09f
Charlie <vanvu25894@gmail.com>
2024-08-22 17:36:38 +0700

    feat: change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'MultiVue',
+    title: 'Charlie Vue',
     htmlAttrs: {
       lang: 'ko'
     },
